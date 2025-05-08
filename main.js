4d6f8da3e3676586c7d19990670bedc3c30b4800
Kylie Banana <banaag.kj@obmontessori.edu.ph>
2025-05-08 11:18:50 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function displayLevelInfo() {
+	var levelEntry = Number(document.getElementById('levelInput').value);
+
+		switch (levelEntry) {
+			case 1:
+				document.getElementById("displayLevel").innerHTML="Level: Easy Mode";
+				break;
+			case 2:
+				document.getElementById("displayLevel").innerHTML="Level: Normal Mode";
+				break;
+			case 3:
+				document.getElementById("displayLevel").innerHTML="Level: Hard Mode";
+				break;
+			default:
+				window.alert("Invalid Input");
+		}
+}
